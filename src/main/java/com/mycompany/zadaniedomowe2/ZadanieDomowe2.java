25503731c7903e451eff6eb7e6c29d5c73a6eb85
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-05-21 12:29:58 +0200

    initial

---
+++ src/main/java/com/mycompany/zadaniedomowe2/ZadanieDomowe2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zadaniedomowe2;
+
+/**
+ *
+ * @author andrz
+ */
+public class ZadanieDomowe2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
